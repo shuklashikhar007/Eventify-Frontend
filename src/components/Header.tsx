b98ed35b5c6197f6f59ff54942be9724f1a21a2b
Tanishq Singh <hello@tanishqsingh.com>
2025-07-07 15:05:13 +0530

    web: import misspelled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./header.css";
+import "./Header.css";
 import { useUserStore } from "@/store/user";
 
 export default function Header() {
